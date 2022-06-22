0df1b8c98ce5785401a0ae19480928b32b910e9c
Marco-Gagliano <brazil_94@hotmail.it>
2022-06-22 15:08:41 +0200

    esercizio-con-bonus/consegna-esercizio

---
+++ resources/views/xbox.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Xbox</title>
+</head>
+<body>
+
+    <header>
+
+        <nav>
+            <a href="{{route('home')}}">Ritorna al menu principale</a>
+            <a href="{{route('playstation')}}">Playstation 5 Menu</a>
+            <a href="{{route('nintendo')}}">Nintendo Switch Menu</a>
+        </nav>
+
+    </header>
+
+    <main>
+
+        <h3>{{$nome}}</h3>
+        <h3>{{$produttore}}</h3>
+
+        <ul>
+        @foreach ($listaGiochi as $giochi)
+            <li>{{$giochi}}</li>
+        @endforeach
+        </ul>
+
+    </main>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,65 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+
+    $data = [
+        'hello' => 'Hello World',
+    ];
+
+    return view('home', $data);
+})->name('home');
+
+
+Route::get('/playstation', function () {
+    $data = [
+        'nome' => 'Playstation 5',
+        'produttore' => 'Sony',
+        'listaGiochi' => [
+            'Horizon Forbidden West',
+            'God Of War Ragnarok',
+            'Gran Turismo 7',
+            'Ratchet and Clank: Rift Apart',
+            'Elder Ring',
+            'Ghost of Tsushima'
+        ]
+    ];
+
+    return view('playstation', $data);
+})->name('playstation');
+
+
+Route::get('/xbox', function () {
+    $data = [
+        'nome' => 'Xbox Series X',
+        'produttore' => 'Microsoft',
+        'listaGiochi' => [
+            'Starfield',
+            'Gears 5',
+            'Forza Horizon 5',
+            'Halo Infinite',
+            'Quantum Break',
+            'Battletoads',
+        ]
+    ];
+    return view('xbox', $data);
+})->name('xbox');
+
+
+Route::get('/nintendo', function () {
+    $data = [
+        'nome' => 'Nintendo Switch',
+        'produttore' => 'Nintendo',
+        'listaGiochi' => [
+            'Bayonetta 2',
+            'Pokemon Spada',
+            'Astral Chain',
+            'Xenoblade Chronicles 2',
+            'Metroid Dread',
+            'Mario Strikers: Battle League Football',
+            'Hyrule Warriors',
+        ]
+    ];
+    return view('nintendo', $data);
+})->name('nintendo');
+
+
